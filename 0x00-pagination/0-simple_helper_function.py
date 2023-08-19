756bcd294ea1d38189d1ee9f9109afd754cdcafc
Dinobi <architectstan01@gmail.com>
2023-08-19 21:52:49 +0100

    Backend Tasks

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+"""Pagination Task"""
 from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     '''a function named index_range that takes two integer arguments page and page_size'''
