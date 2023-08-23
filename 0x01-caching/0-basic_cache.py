6dd77416455369142c8008bdb38e345246eee917
Dinobi <architectstan01@gmail.com>
2023-08-23 17:29:27 +0100

    Python Caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,14 +5,17 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     """Inherites from Basecaching"""
     def __init__(self):
+        """Initializes the class"""
         super().__init__()
         self.cache_data = dict()
 
     def put(self, key, item):
+        """Allocates a given key to an item"""
         if key or item is None:
             pass
         self.cache_data[key] = item
     def get(self, key):
+        """Gets the value of a given key"""
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
