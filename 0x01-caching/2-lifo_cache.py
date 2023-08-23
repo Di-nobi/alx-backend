c48f01793d7f54166e9c13a53e34d65087f7a4a9
Dinobi <architectstan01@gmail.com>
2023-08-23 17:38:32 +0100

    Python Caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -3,10 +3,13 @@
 from base_caching import BaseCaching
 
 class LIFOCache(BaseCaching):
+    """Inherites from Basecaching"""
     def __init__(self):
+        """Initializes the class"""
         super().__init__()
 
     def put(self, key, item):
+        """Allocates a given key to an item"""
         if key and item: 
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data.keys():
                 discard = list(self.cache_data.keys())[-1]
@@ -14,6 +17,7 @@ class LIFOCache(BaseCaching):
                 print(f"DISCARD: {discard}")
             self.cache_data[key] = item
     def get(self, key):
+        """Gets the value of a given key"""
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
