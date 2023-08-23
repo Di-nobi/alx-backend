96f16c28cf7a46e5ba1b395ac5bf8565e9b8f0bf
Dinobi <architectstan01@gmail.com>
2023-08-23 17:17:30 +0100

    Caching in Python

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
-"""FIFO Caching"""
+#!/usr/bin/env python3
+"""LIFO Caching"""
 from base_caching import BaseCaching
 
 class FIFOCache(BaseCaching):
@@ -9,7 +9,7 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         if key and item: 
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.cache_data.keys():
-                discard = list(self.cache_data.keys())[0]
+                discard = list(self.cache_data.keys())[-1]
                 del self.cache_data[discard]
                 print(f"DISCARD: {discard}")
             self.cache_data[key] = item
