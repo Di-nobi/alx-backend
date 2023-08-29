1a5f5e5b870bf1b19e2cb7f95fe2003d72a4b56d
Dinobi <architectstan01@gmail.com>
2023-08-29 23:56:29 +0100

    Backend i18N

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Flask Application"""
+from flask_babel import Babel
+from flask import Flask, request
+from flask import render_template
+from flask_babel import _
+
+class Config(object):
+    """Class Configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+    
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+
+app.route('/', strict_slashes=False)
+def index():
+    """Index of a file"""
+    return render_template('3-index.html')
+
+@babel.localeselector
+def get_locale():
+    """Gets the locale selector"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+if __name__ == "__main__":
+    app.run(port='5000', host='0.0.0.0', debug=True)
